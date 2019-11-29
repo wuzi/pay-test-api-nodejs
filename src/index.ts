3e8cbae7cf3b3db5c57717bb45b32857eaa66843
Lucas Godoy <lcsdegodoy@outlook.com>
2019-11-29 13:18:45 -0300

    feat: add koa server

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,14 @@
+import Koa from 'koa';
+import cors from '@koa/cors';
+import bodyParser from 'koa-bodyparser';
 import dotenv from 'dotenv';
+import logger from './utils/logger';
 dotenv.config();
+
+const app = new Koa();
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(bodyParser());
+
+app.listen(port, (): void => logger('index', `Server started at http://localhost:${port}`, 'info'));
